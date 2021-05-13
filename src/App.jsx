8f101400103e21e6674eefc9da9819b56a805287
Oliver Kovalcik <oliver.kovalcik@gmail.com>
2021-05-13 10:03:40 +0200

    Everything Works

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { ChakraProvider } from "@chakra-ui/react"
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Home } from "./pages/Home";
 
-import {Home} from "./pages/Home.jsx"
-
-export const App = () => (<ChakraProvider>
- <Home/>
-</ChakraProvider>) 
-
+export const App = () => (
+  <ChakraProvider>
+    <Home />
+  </ChakraProvider>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from "react"
-import { Heading, Box, Center } from "@chakra-ui/react"
-
+import React from "react";
+import { Box, Center, Heading, Button } from "@chakra-ui/react";
 
 export const Home = () => (
- <Box backgroundImage="url('src/assets/image.jpg') "
-  h="100vh"
-  backgroundPosition="cover"
-  backgroundRepeat="no-repeat"
- >
-  <Center>
-   <Heading>HomePage</Heading>
-  </Center>
- </Box>
-)
+  <Box backgroundImage="url('src/assets/image.jpg')" h="100vh">
+    <Center h="100vh">
+      <div>
+        <Heading color="black" textAlign="center" mb="4">
+          Welcome
+        </Heading>
+        <Button>For More Info</Button>
+      </div>
+    </Center>
+  </Box>
+);
