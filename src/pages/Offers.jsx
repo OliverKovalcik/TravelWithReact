d7edb7a783b6b0fd86f611127671f646a1d62788
Oliver Kovalcik <oliver.kovalcik@gmail.com>
2021-05-16 12:47:56 +0200

    Add FilterNew

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -12,7 +12,7 @@ import { FilterCards } from './components/FilterCards'
 
 export const Offers = () => {
   const { data, error, isLoading } = useOffers()
-
+  const [newCards, setNewCards] = React.useState(false)
   const toast = useToast()
   if (error) {
     toast({ status: 'error', title: 'something wrong ', description: 'sorry' })
@@ -24,13 +24,35 @@ export const Offers = () => {
       <HomeInput />
       <Grid templateColumns="repeat(4, 1fr)" gap={6}>
         <GridItem rowSpan={3}>
-          <FilterNew />
+          <FilterNew newCards={newCards} setNewCards={setNewCards} />
           {sortBy(uniqBy(data, 'country'), ['country']).map(({ id, country }) => {
             return <FilterCards key={id} country={country} />
           })}
         </GridItem>
+
+        {/* eslint-disable-next-line no-nested-ternary */}
         {isLoading ? (
           <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+        ) : newCards ? (
+          data
+            .filter((items) => isItemNew(items.createdAt) === true)
+            .map(
+              ({ id, thumbnail, nights, city, country, price, rating, reviewCount, createdAt }) => {
+                return (
+                  <Card
+                    key={id}
+                    imageUrl={thumbnail}
+                    numberOfNights={nights}
+                    destination={`${city}, ${country}`}
+                    formatedPrice={formatPrice(price)}
+                    rating={rating}
+                    reviewsCount={reviewCount}
+                    linkTo="/offers/1"
+                    isNew={isItemNew(createdAt)}
+                  />
+                )
+              }
+            )
         ) : (
           data.map(
             ({ id, thumbnail, nights, city, country, price, rating, reviewCount, createdAt }) => {
--- src/pages/components/FilterNew.jsx
+++ src/pages/components/FilterNew.jsx
@@ -1,16 +1,24 @@
 import * as React from 'react'
 import { Checkbox, Heading } from '@chakra-ui/react'
+import PropTypes from 'prop-types'
 
-export const FilterNew = () => {
+export const FilterNew = ({ newCards, setNewCards }) => {
   return (
     <>
       <Heading paddingLeft="20px" m="10px" size="sm">
         Filters
       </Heading>
-      <Checkbox paddingLeft="30px">New</Checkbox>
+      <Checkbox paddingLeft="30px" onChange={() => setNewCards(!newCards)}>
+        New
+      </Checkbox>
       <Heading paddingLeft="20px" m="10px" size="sm">
         FILTER BY COUNTRY
       </Heading>
     </>
   )
+}
+
+FilterNew.propTypes = {
+  newCards: PropTypes.bool,
+  setNewCards: PropTypes.func,
 }
